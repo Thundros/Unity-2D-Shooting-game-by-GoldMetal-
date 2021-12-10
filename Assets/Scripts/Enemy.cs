0438076b6e25e275892d02e6569d0337ad7aa437
Daegwon Kim <kdg1449@naver.com>
2021-12-10 09:24:02 +0900

    third commit
    add Enemy

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float speed;
+    public int health;
+    public Sprite[] sprites;
+
+    Rigidbody2D rigid;
+    SpriteRenderer spriteRenderer;
+
+    void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        rigid = GetComponent<Rigidbody2D>();
+        rigid.velocity = Vector2.down * speed;
+    }
+
+    void OnHit(int dmg)
+    {
+        health -= dmg;
+        spriteRenderer.sprite = sprites[1];
+        
+        // Delay to sprite change
+        Invoke("ReturnSprite", 0.1f);
+
+        if (health <= 0)
+            Destroy(gameObject);
+    }
+
+    void ReturnSprite()
+    {
+        spriteRenderer.sprite = sprites[0];
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "BorderBullet")
+            Destroy(gameObject);
+        else if (collision.gameObject.tag == "PlayerBullet")
+        {
+            Bullet bullet = collision.gameObject.GetComponent<Bullet>();
+            OnHit(bullet.dmg);
+
+            Destroy(collision.gameObject);
+        }
+    }
+}
